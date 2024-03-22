e7215dade96d15c7067888fca2dc2e802c06aac3
ViktorVelizarov <VVVelizarov18@codingburgas.bg>
2024-03-22 11:39:46 +0100

    style the sneaker cards and put them in a grid responsive to the window size

--- components/SneakersList.jsx
+++ components/SneakersList.jsx
@@ -1,3 +1,4 @@
+
 const getSneakers = async () => {
   try {
     const res = await fetch("http://localhost:3000/api/sneakers", {
@@ -19,20 +20,30 @@ export default async function SneakersList() {
   const { sneakers } = await getSneakers();
 
   return (
-    <>
-      Sneakers List:
+    <div className="grid grid-cols-3 gap-4 ml-14 mr-14">
       {sneakers && sneakers.map((t) => (
         <div
           key={t._id}
-          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+          className="border border-slate-300 my-3 flex flex-col justify-between items-start p-4"
         >
+          <img className="w-full h-full" src={t.picture} alt={t.title} />
           <div>
-            <h2 className="font-bold text-2xl text-yellow-200">{t.title}</h2>
-            <img className="w-20 h-20 "src={t.picture} alt={t.title} />
-            <div>{t.description}</div>
+            <h2 className="font-bold text-lg">{t.title}</h2>
+            <p className='text-gray-500 font-medium'>{t.type}</p>
+            <p className='text-gray-500 font-medium'>{t.category}</p>
+            {t.sale !== "no" ? (
+              <div className="flex flex-row">
+                <p className='mt-3 font-semibold text-green-500'>
+                  Sale Price: ${(t.price - (t.price * t.sale / 100)).toFixed(2)}
+                </p>
+                <p className='mt-3 ml-2 line-through'>${t.price}</p>
+              </div>
+            ) : (
+              <p className='mt-3 font-semibold'>Price: ${t.price}</p>
+            )}
           </div>
         </div>
       ))}
-    </>
+    </div>
   );
 }
